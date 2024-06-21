6a84d1ec402ff2f4b1a18553378b878d3caa234c
mostefa <mostefa.aliarous@gmail.com>
2024-06-21 18:43:54 +0100

    lint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,7 +6,7 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly userService: UserService) { } // Inject UserService
+    constructor(private readonly userService: UserService) { } 
 
     async authenticate(authenticateDto: LoginUserDto) {
         const user = await this.userService.findUserByEmail(authenticateDto.email);
