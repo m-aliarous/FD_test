47204a3ba195539326b6e6cc1f590e4d8ebad5b6
mostefa <mostefa.aliarous@gmail.com>
2024-06-21 19:07:10 +0100

    fix

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,9 +10,10 @@ export class AuthService {
 
     async authenticate(authenticateDto: LoginUserDto) {
         const user = await this.userService.findUserByEmail(authenticateDto.email);
+        if (!user)  throw new NotFoundException('Invalid credentials');
         const isMatch = await bcrypt.compare(authenticateDto.password, user.hashedPassword);
-        if (!user || !isMatch)  throw new NotFoundException('Invalid credentials');
+        if (!isMatch)  throw new NotFoundException('Invalid credentials');
         const token = sign({ ...user }, 'secrete');
-        return { token, user };
+        return {token};
     }
 }
