82d9d024f69b04b783d06fca735474da06888c8c
mostefa <mostefa.aliarous@gmail.com>
2024-06-21 18:22:13 +0100

    login API

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginUserDto } from 'src/user/dtos/loginUserDto ';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post()
+    async login(@Res() res, @Body() authenticateDto: LoginUserDto) {
+      try {
+        const response = await this.authService.authenticate(authenticateDto);
+        return res.status(HttpStatus.OK).json({ response });
+      } catch (error) {
+        return res.status(error.status).json(error.response);
+      }
+    }
+  
+
+
+  }
