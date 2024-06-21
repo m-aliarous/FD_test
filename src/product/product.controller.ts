491a0579c9168f383231c04494369e6cafd22b53
mostefa <mostefa.aliarous@gmail.com>
2024-06-21 14:49:04 +0100

    added logic and Apis to create and list all products

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { ProductService } from './product.service';
+import Product from './entities/product';
+import { CreateProductDto } from './dtos/createProductDto';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+    constructor(private readonly productService: ProductService) {}
+
+    @Get()
+    async getAllProducts(): Promise<Product[]> {
+      const products = await this.productService.getAllProducts();
+      return products;
+    }
+
+    @Post()
+    async createProduct(@Body() createProductDto: CreateProductDto) {
+      const newProduct = await this.productService.createProduct(createProductDto);
+      return newProduct;
+    }
+}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,11 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import Product from './entities/product';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateProductDto } from './dtos/createProductDto';
 @Injectable()
 export class ProductService {
     constructor(
         @InjectRepository(Product)
-        private userRepository: Repository<Product>,
+        private productRepository: Repository<Product>,
       ) {}
+
+//      Create Product
+      async createProduct(createProductDto?: CreateProductDto) {
+        if (!createProductDto) {
+            throw new HttpException('CreateProductDto is undefined', HttpStatus.BAD_REQUEST);
+          }
+        const { name,description, price, category,image } = createProductDto;
+        const newProduct = await this.productRepository.create({name,description, price, category,image});
+        await this.productRepository.save(newProduct);
+        return newProduct;
+      }
+//      Return All products
+      async getAllProducts() {
+        const products = this.productRepository.find();
+        return products;
+      }
 }
