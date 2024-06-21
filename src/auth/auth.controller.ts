f3697e1cbe1505f3ed18f7c906d03c192de18907
mostefa <mostefa.aliarous@gmail.com>
2024-06-21 18:44:50 +0100

    profile api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,26 @@
-import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from 'src/user/dtos/loginUserDto ';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+    constructor(private readonly authService: AuthService) { }
 
     @Post()
     async login(@Res() res, @Body() authenticateDto: LoginUserDto) {
-      try {
-        const response = await this.authService.authenticate(authenticateDto);
-        return res.status(HttpStatus.OK).json({ response });
-      } catch (error) {
-        return res.status(error.status).json(error.response);
-      }
+        try {
+            const response = await this.authService.authenticate(authenticateDto);
+            return res.status(HttpStatus.OK).json({ response });;
+        } catch (error) {
+            throw new HttpException(error.response, error.status || HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
-  
 
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    profile(@Req() req, @Res() res) {
+        return res.status(HttpStatus.OK).json(req.user);
+    }
 
-  }
+}
