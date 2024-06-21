f4a061e2b9eed7a5339e7a5450499f9155a06b0c
mostefa <mostefa.aliarous@gmail.com>
2024-06-21 17:45:11 +0100

    register a  user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dtos/createUserDto';
+import User from './entities/user';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    constructor(private readonly userService: UserService) {}
+    
+    @Post()
+    async createUser(@Body() createUserDto: CreateUserDto) {
+      const newUser = await this.userService.createuser(createUserDto);
+      return newUser;
+    }
+    @Get()
+    async getAllUsers(): Promise<User[]> {
+        const users = await this.userService.getAllUsers();
+        return users;
+      }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,65 @@
-import { Injectable } from '@nestjs/common';
-
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import User from './entities/user';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dtos/createUserDto';
+import * as bcrypt from 'bcrypt';
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+    ) { }
+
+    async getAllUsers() {
+        const users = this.userRepository.find();
+        return users;
+    }
+
+    async findUserByEmail(email: string): Promise<User | undefined> {
+        return await this.userRepository.findOne({
+            where: {
+                email: email,
+            },
+        });
+    }
+
+    async getUserByEmail(email: string) {
+        const user = this.findUserByEmail(email);
+        if (user) {
+            return user;
+        }
+        throw new NotFoundException('Could not find the user');
+    }
+
+
+    async getUserById(id: number) {
+        const user = await this.userRepository.findOne({
+            where: {
+                id: id,
+            },
+        });
+        if (user) {
+            return user;
+        }
+        throw new NotFoundException('Could not find the user');
+    }
+
+
+    async createuser(createUserDto?: CreateUserDto) {
+        if (!createUserDto) {
+            throw new HttpException('createUserDto is undefined', HttpStatus.BAD_REQUEST);
+        }
+        const { firstName, lastName, email, password } = createUserDto;
+
+        const existingUser = await this.findUserByEmail(email);
+        if (existingUser) {
+            throw new HttpException('User with this email already exists', HttpStatus.BAD_REQUEST);
+        }
+        
+        const hashedPassword = await bcrypt.hash(password, 10); //hashing the password
+        const newuser = await this.userRepository.create({ firstName, lastName, email, hashedPassword });
+        await this.userRepository.save(newuser);
+        return newuser;
+    }
+}
